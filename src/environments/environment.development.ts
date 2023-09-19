265962e29b34f9527d4c41437c1e108b9407a7eb
Larejun <lmp.solns@gmail.com>
2023-09-19 14:21:26 +0800

    Initial files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  unslpashApi: 'https://api.unsplash.com',
+  unslpashAccessKey: 'VALtzn58KWy_ArALvcGAH1W8UWbJcIKTdMUrRtBv6eM',
+  unslpashSecretKey: 'IbzR8nlMDTiy-Wb02vMUUJBnJDNreXzPj2x_Mqwa4Pc'
+};
